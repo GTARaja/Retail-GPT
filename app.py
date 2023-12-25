8bcb5fedde51a1082a71a1f37487a893bf1f2e3a
Raj Vijay <rajgtaraja@gmail.com>
2023-12-25 22:46:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,74 +1,152 @@
+#import openai
 import streamlit as st
-from PyPDF2 import PdfReader
+import google.generativeai as genai
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-import google.generativeai as palm
-from langchain.embeddings import GooglePalmEmbeddings
-from langchain.llms import GooglePalm
-from langchain_google_genai.llms import GoogleGenerativeAI
-from langchain.vectorstores import FAISS
-from langchain.chains import ConversationalRetrievalChain
-from langchain.memory import ConversationBufferMemory
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_core.prompts import PromptTemplate
+from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
+from streamlit_chat import message
+from dotenv import load_dotenv
 import os
+from langchain.vectorstores import Chroma
+import openai
+from langchain.document_loaders import UnstructuredMarkdownLoader
+from langchain.chains.question_answering import load_qa_chain
+from langchain.document_loaders import UnstructuredPDFLoader
 
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
+#load_dotenv()
+
+for m in genai.list_models():
+  if 'generateContent' in m.supported_generation_methods:
+    print(m.name)
+model = genai.GenerativeModel('gemini-pro')
+persist_directory = 'chromadb_oconversion'
 
 
-def get_pdf_text(pdf_docs):
-    text=""
-    for pdf in pdf_docs:
-        pdf_reader= PdfReader(pdf)
-        for page in pdf_reader.pages:
-            text+= page.extract_text()
-    return  text
 
-def get_text_chunks(text):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
-    chunks = text_splitter.split_text(text)
-    return chunks
+print('embedding the document now')
 
-def get_vector_store(text_chunks):
+st.title("🤖 RetailGPT - Your Retail Guide..")
+
+with st.chat_message("user"):
+    st.write("Hello User 👋 : Please wait while we initialize RetailGPT !")
+print("Here!!!")
+
+if "messages" not in st.session_state.keys(): # Initialize the chat message history
+    st.session_state.messages = [
+        {"role": "assistant", "content": "RetailGPT is ready! Shoot your questions"}
+    ]
+
+def get_text():
+    input_text = st.text_input("", key="input")
+    return input_text
+
+def prepare():
+    loader = PyPDFDirectoryLoader("oconversion")
+    data = loader.load_and_split()
+    print(data)
+    persist_directory = 'chromadb_oconversion'
+    context = "\n".join(str(p.page_content) for p in data)
+    print("The total number of words in the context:", len(context))
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=200)
+    context = "\n\n".join(str(p.page_content) for p in data)
+    texts = text_splitter.split_text(context)
+    # vectordb = Chroma.from_documents(documents=texts, embedding=embeddings, persist_directory=persist_directory)
+    #vectordb = Chroma.from_documents(documents=pages, embedding=embeddings, persist_directory=persist_directory)
+    # vectordb.persist()
     embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
-    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
-    return vector_store
+    vector_index = Chroma.from_texts(texts, embeddings,persist_directory=persist_directory)
+    vector_index.persist()
+    vector_index = Chroma(persist_directory=persist_directory, embedding_function=embeddings)
+    return vector_index
 
-def get_conversational_chain(vector_store):
-    llm=GoogleGenerativeAI(model="gemini-pro",
-                             temperature=0.3)
-    memory = ConversationBufferMemory(memory_key = "chat_history", return_messages=True)
-    conversation_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=vector_store.as_retriever(), memory=memory)
-    return conversation_chain
+def load_chroma():
+    with st.spinner(text="Loading indexed Retail Documents ! This should take 1-2 minutes."):
+        persist_directory = 'chromadb_oconversion'
+        embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+        vector_index = Chroma(persist_directory=persist_directory, embedding_function=embeddings)
+    return vector_index
 
-def user_input(user_question):
-    response = st.session_state.conversation({'question': user_question})
-    st.session_state.chatHistory = response['chat_history']
-    for i, message in enumerate(st.session_state.chatHistory):
-        if i%2 == 0:
-            st.write("You : ", message.content)
-        else:
-            st.write("RetailBOT : ", message.content)
-def main():
-    st.set_page_config("Retail GPT - Your Retail Guide")
-    st.header("RetailGPT - Your everyday guide 💬")
-    user_question = st.text_input("Ask your query and our BOT will respond")
-    if "conversation" not in st.session_state:
-        st.session_state.conversation = None
-    if "chatHistory" not in st.session_state:
-        st.session_state.chatHistory = None
-    if user_question:
-        user_input(user_question)
-    with st.sidebar:
-        st.title("Settings")
-        st.subheader("Upload your Documents")
-        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Process Button", accept_multiple_files=True)
-        if st.button("Process"):
-            with st.spinner("Processing"):
-                raw_text = get_pdf_text(pdf_docs)
-                text_chunks = get_text_chunks(raw_text)
-                vector_store = get_vector_store(text_chunks)
-                st.session_state.conversation = get_conversational_chain(vector_store)
-                st.success("Done")
+vectordb=load_chroma()
+def search_chroma(vectordb,question):
+    #result_docs = vectordb.similarity_search(query)
+
+    # vectordb = Chroma(persist_directory=persist_directory, embedding_function=embeddings)
+
+    print("Raj"+question+"Vijay")
+
+    docs = vectordb.similarity_search(question)
+    prompt_template = """
+      Answer the question as detailed as possible from the provided context, make sure to provide all the details\n\n
+      Context:\n {context}?\n
+      Question: \n{question}\n
+
+      Answer:
+    """
+
+    prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+    model = ChatGoogleGenerativeAI(model="gemini-pro",
+                                   temperature=1.0, apikey='AIzaSyD0W_tAHffQRKH5_sZMa_Tuy0NJfrsDK8E')
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+    print("Raj2" + question + "Vijay2")
+    print(docs)
+    response = chain({"input_documents": docs, "question": question},return_only_outputs=True)
+    #print(output)
+    print(response['output_text'])
+    return response['output_text']
+
+#question = get_text()
+#if question:
+    #output = search_chroma(vectordb,question)
+    #st.session_state.past.append(question)
+    #st.session_state.generated.append(output)
+
+#
+if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+for message in st.session_state.messages: # Display the prior chat messages
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            #response = chat_engine.chat(prompt)
+            output = search_chroma(vectordb, prompt)
+            st.write(output)
+            message = {"role": "assistant", "content": output}
+            st.session_state.messages.append(message) # Add response to message history
 
 
 
-if __name__ == "__main__":
-    main()
+
+footer="""<style>
+a:link , a:visited{
+color: blue;
+background-color: transparent;
+text-decoration: underline;
+}
+
+a:hover,  a:active {
+color: red;
+background-color: transparent;
+text-decoration: underline;
+}
+
+.footer {
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+background-color: white;
+color: black;
+text-align: center;
+}
+</style>
+<div class="footer">
+<p>Developed with ❤ by <a style='display: block; text-align: center;'target="_blank">Raj "GTARaja" Vijay</a></p>
+
+</div>
+"""
+st.markdown(footer,unsafe_allow_html=True)
